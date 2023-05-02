5b1460b4bf8b995f84ac34c683869b4116f66a5a
ratdoux <62392831+ratdoux@users.noreply.github.com>
2023-05-02 11:54:36 +0200

    fix run.bat script

--- run.bat
+++ run.bat
@@ -1,9 +1,24 @@
 @echo off
-copy .\websocketd.exe ..\AutoGPT\websocketd.exe
-echo websocketd.exe >> ..\AutoGPT\.gitignore
+setlocal enabledelayedexpansion
+
+copy .\websocketd.exe ..\Auto-GPT\websocketd.exe
+echo websocketd.exe >> ..\Auto-GPT\.gitignore
+
+set "first_line_processed=false"
+(for /F "tokens=*" %%A in (..\Auto-GPT\run.bat) do (
+    if not "!first_line_processed!"=="true" (
+        echo @echo off > ..\Auto-GPT\temp_run.bat
+        echo SET USERPROFILE=%%HOMEDRIVE%%%%HOMEPATH%% >> ..\Auto-GPT\temp_run.bat
+        set "first_line_processed=true"
+    ) else (
+        echo %%A >> ..\Auto-GPT\temp_run.bat
+    )
+))
+move /Y ..\Auto-GPT\temp_run.bat ..\Auto-GPT\run.bat
+
 cd ..
-cd AutoGPT
+cd Auto-GPT
 start cmd.exe /k "websocketd --address=localhost --loglevel=debug --binary=true --port=8080 run.bat"
 cd ..
 cd AutoGPT-UI
-start cmd.exe /k "npm start"
+start cmd.exe /k "npm install && npm start"
